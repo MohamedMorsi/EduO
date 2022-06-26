f952e8cca6e8ce63062d88270989d2ad85f8c54c
Mohamed Morsi <mmm@crossworkers.com>
2022-06-26 20:06:26 +0200

    add_AuthStateProvider

---
+++ EduO.Web/Program.cs
@@ -0,0 +1,16 @@
+using EduO.Web;
+using EduO.Web.AuthProviders;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
